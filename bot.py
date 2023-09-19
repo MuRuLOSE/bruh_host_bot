859ba0757fd9ed8d0a43b8591cb480201e9c6fb3
Macsim <podryazhchik@mail.ru>
2023-09-19 13:13:52 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import sqlite3
 import paramiko
 import hashlib
 from urllib.parse import urlencode
-from hello import gen_banner
+from utils import hello
 
 from aiogram.types import UserProfilePhotos
 from aiogram import Bot, Dispatcher, Router, types, F
@@ -133,14 +133,14 @@ async def command_start_handler(message: Message) -> None:
     now = datetime.datetime.now(tz=pytz.timezone('Europe/Moscow'))
     
     hi = ""
-    if now.hour < 5 or now.hour >= 20:
+    if now.hour >= 0 and now.hour >= 4 and now.hour < 5:
         hi = ["Доброй ночи","Спокойной ночи", "Приятных снов","Хорошего дня"]
 
-    elif now.hour < 13:
-        hi = ["Доброе утро","Добрейшего утречка","Светлого утра","Солнечного дня"]
+    elif now.hour >= 5 and now.hour < 13:
+        hi = ["Доброе утро","Добрейшего утречка","Светлого утра","Солнечного дня", "Чудесного утра"]
 
-    elif now.hour < 18:
-        hi = ["Добрый день", "Хорошего дня", "Удачного дня"]
+    elif now.hour >= 13 and now.hour <= 17:
+        hi = ["Добрый день", "Хорошего дня", "Удачного дня", "Счастливого дня", "Спасибо что ты тут именно днём", "Днём ты выглядишь лучше"]
 
     else:
         hi = ["Добрый вечер", "Приятного вечера","Приятного отдыха"]
@@ -151,7 +151,7 @@ async def command_start_handler(message: Message) -> None:
         file = await bot.get_file(user_profile_photo.photos[0][0].file_id)
         await bot.download_file(file.file_path, f'avatars/avatar_{message.from_user.id}.png')
 
-        await message.answer_photo(photo=gen_banner(username=message.from_user.first_name,user_id=message.from_user.id,hi=random.choice(hi),avatar=f"avatars/avatar_{message.from_user.id}"),caption=f'<b>Наш хостинг стабильный, дешевый и имеет свои преймущества.</b> #{git[:7]}',reply_markup=start(message.from_user.id))
+        await message.answer_photo(photo=hello.gen_banner(username=message.from_user.first_name,user_id=message.from_user.id,hi=random.choice(hi),avatar=f"avatars/avatar_{message.from_user.id}.png"),caption=f'<b>Наш хостинг стабильный, дешевый и имеет свои преймущества.</b> #{git[:7]}',reply_markup=start(message.from_user.id))
     else:
         await message.answer(f'<b>{random.choice(hi)}</b>, <i>{message.from_user.first_name}</i>\n<b>Наш хостинг стабильный, дешевый и имеет свои преймущества.</b> #{git[:7]}',reply_markup=start(message.from_user.id))
     
