490d6b1edc8c791f7455d1c521595c8f76a60019
Macsim <podryazhchik@mail.ru>
2023-09-20 14:44:07 +0300

    что

--- utils/hello.py
+++ utils/hello.py
@@ -71,3 +71,4 @@ def gen_banner(hi,username,user_id,avatar):
     # Сохраняем изображение
     image.save(f"avatars/result_image_{user_id}.png")
     return input_file.FSInputFile(path=f"avatars/result_image_{user_id}.png")
+    # n
